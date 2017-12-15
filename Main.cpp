5b74e034b52fca6464cdd205974f883cd5a9e866
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-12-15 23:13:40 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,10 @@
 #include<fstream>
 using namespace std;
 
-void cr(int**& c, int& m, int& n, int argc, char* argv[]){
-    int i, j, k, d;
+void cr(int**& matrix, int& m, int& n, int argc, char* argv[]){
+    int i, j, k;
     k = 2;
-    string str1, str2, prom;
+    string str1, str2;
     for (i=0; (argv[1][i] != 'x') && (argv[1][i] != 'X'); i++)
         str1 += argv[1][i];
     for (i += 1; i < strlen(argv[1]); i++)
@@ -17,34 +17,34 @@ void cr(int**& c, int& m, int& n, int argc, char* argv[]){
     m = atoi(str1.c_str());
     n = atoi(str2.c_str());
     if (m==0 || n==0)
-        cout << "Неверная матрица, повторите ввод" << endl;
-    c = new int*[m];
-    for (i = 0; i < m; i++)
-        c[i] = new int [n];
-    for (i = 0; i < m; i++)
-        for (j = 0; j < n; j++)
-            c[i][j]=0;
-    if (argc==3) {
-	string prom;
-	d=0;
-	for (i = 0; i < m; i++)
-		for (j = 0; j < n; j++) {
-			for(; d<strlen(argv[2]); d++) {
-				if(argv[2][d]>='0' && argv[2][d]<='9') {
-					prom = argv[2][d];
-					c[i][j] = c[i][j]*10+atoi( prom.c_str() );
-				} else if(argv[2][d] == ','){
-				    d++;
+        cout << "Неверный размер матрицы, повторите ввод" << endl;
+    matrix = new int *[m];
+    for (i = 0; i<m; i++)
+        matrix[i] = new int [n];
+    for (i = 0; i<m; i++)
+        for (j = 0; j<n; j++)
+            matrix[i][j] = 0;
+    if (argc == 3) {
+	string str3;
+	int z=0;
+	for (i=0; i<m; i++)
+		for (j=0; j<n; j++) {
+			for (; z < strlen(argv[2]); z++) {
+				if (argv[2][z]>='0' && argv[2][z]<='9') {
+					str3 = argv[2][z];
+					matrix[i][j] = matrix[i][j]*10 + atoi(str3.c_str());
+				} else if (argv[2][z] == ',') {
+				    z++;
 				    break;
 				}
 
 			}
 		}
 }
-    for (i = 0; i < m; i++)
-        for (j = 0; j < n; j++, k++){
-            if (k < argc )
-                c[i][j] = atoi(argv[k]);
+    for (i = 0; i<m; i++)
+        for (j = 0; j<n; j++, k++){
+            if (k < argc)
+                matrix[i][j] = atoi(argv[k]);
         }
 
 }
@@ -58,102 +58,109 @@ void menu(){
     cout << "5. Сохранить в файл" << endl;
     cout << "6. Загрузить из файла" << endl;
     cout << "7. Сортировать матрицу" << endl;
-    cout << "8. завершить работу программы" << endl;
+    cout << "8. Завершить работу программы" << endl;
 }
 
-void out(int**c, int m, int n){
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++)
-            cout << c[i][j] << " ";
-        cout<< endl;}
+void out(int**matrix, int m, int n){
+    for (int i=0; i<m; i++){
+        for (int j=0; j<n; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;}
 }
 
-void add(int**&c, int m, int n)
+void add(int**& matrix, int m, int n)
 {
-    int dopmat, i, j;
-    cout << "Введите матрицу " << a << "x" << b << endl;
-    for(i = 0; i < m; i++){
-        for(j = 0; j < n; j++){
-        cin >> dopmat;
-        c[i][j] = c[i][j] + dopmat;}
+    int matrix2, i, j;
+    cout << "Введите матрицу " << m << "x" << n << endl;
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+        cin >> matrix2;
+        c[i][j] = c[i][j] + matrix2; 
+	}
     }
     cout << endl;
-    for( i=0; i < m; i++){
-        for(j=0; j < n; j++){
-        cout << c[i][j] << " ";}
+    for (i=0; i<m; i++){
+        for (j=0; j<n; j++){
+        cout << matrix[i][j] << " ";
+	}
     cout << endl;
     }
 }
 
-void mul(int**&c, int &m, int &n)
+void tran(int**& matrix, int &m, int &n)
 {
-    if (c == nullptr){
-    cout << " Пустая матрица ";
-    return;
+    int i, j;
+    int **matrix2 = nullptr;
+    matrix2 = new int*[n];
+    for (i=0; i<n; i++)
+    matrix2[i] = new int[m];
+    for (i=0; i<n; i++)
+        for (j=0; j<m; j++)
+        matrix2[i][j] = matrix[j][i];
+        for (i=0; i<m; i++)
+            delete[] matrix[i];
+        delete[] matrix;
+    matrix = matrix2;
+    swap (m, n);
 }
-    string cs;
-    int m1=0;
-    int n1=0;
-    cout << "Введите размер матрицы" << endl;
-    cin >> cs;
-    m1 = atoi(cs.c_str());
 
-}
-
-void stf(int**c, int m, int n)
+void stf(int** matrix, int m, int n)
 {
     int i, j;
     ofstream file ("matrix.txt");
-    file << m << "x" << n << endl;
+    file << m << " " << n << endl;
     for (i=0; i < m; i++){
         for (j=0; j < n; j++)
-        file << c[i][j] << " ";
+        file << matrix[i][j] << " ";
         file << endl;
     }
     file.close();
 }
 
-void lff(int**&c, int &m, int &n)
+
+void lff(int**& matrix, int &m, int &n)
 {
     int i, j;
-    ifstream filein("matrix.txt");
-    if (filein.is_open()) {
-        filein >> m >> n;
-        c = new int*[m];
+    ifstream fin("matrix.txt");
+    if (fin.is_open()) {
+        fin >> m >> n;
+        if (mutrix != nullptr){
+        for (int i=0; i<m; i++)
+        delete [] matrix[i];
+        delete [] matrix;
+        }
+        matrix = new int*[m];
         for (i=0; i<m; i++)
         c[i] = new int[n];
         for (i=0; i<m; i++)
-            for(j=0; j<n; j++)
-            filein >> c[i][j];
-        filein.close();
+            for (j=0; j<n; j++)
+            fin >> matrix[i][j];
+        fin.close();
     }
-    else cout << " Файл не найден ";
+    else cout << "Файл не найден";
 }
 
-int main(int argc, char* argv[]){
+int main (int argc, char* argv[]){
     setlocale(LC_ALL, "Russian");
-    int** c = nullptr;
+    int** matrix = nullptr;
     int m, n;
     int choise;
-    if (argc<3){
+    if (argc < 3){
     cout << "Hеверное количество элементов" << endl;
-    return 0;}
-    cr(c, m, n, argc, argv);
-    while(true){
+    return 0;
+    }
+    cr (matrix, m, n, argc, argv);
+    while (true) {
         menu();
         cin >> choise;
-        switch (choise){
-            case 1:
-            out (c, m, n);
+        switch (choise) {
+            case 1: out (matrix, m, n);
             break;
-            case 2:
-            add (c, m, n);
+            case 2: add (matrix, m, n);
             break;
-            case 5:
-            stf(c, m, n);
+            case 5: stf(matrix, m, n);
             break;
-            case 6:
-            lff(c, m, n);
+	    case 6: lff(matrix, m, n);
             break;
             case 8:
               return 0;
